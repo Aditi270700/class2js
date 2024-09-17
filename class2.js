1ba7406c0075ec0542a07b6974d2b9df8113f2fe
aditi saudagar <aditisaudagar17@gmail.com>
2024-09-17 12:32:04 +0530

    first commit

---
+++ class2.js
@@ -0,0 +1,17 @@
+console.log("This is second class");
+function p35(){
+    // console.log("function called");
+    // let num1 = 90;
+    // let num2 = 100;
+    // console.log(num1+num2);
+    let num1 = parseInt(window.prompt("Enter number 1 "));
+    console.log(num1)
+    console.log(typeof(num1))
+
+    let num2 = parseInt(window.prompt("Enter number 2 "));
+    console.log(num2)
+    console.log(typeof(num2))
+
+    console.log(num1+num2);
+}
+p35();
